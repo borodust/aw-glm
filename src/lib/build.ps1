f2ae21b805bf1446bc3e446281f9af1f39bed1f8
Pavel Korolev <dev@borodust.org>
2021-10-04 21:40:50 +0300

    Add win build

---
+++ src/lib/build.ps1
@@ -0,0 +1,38 @@
+param (
+    [parameter(Mandatory=$false)]
+    [int] $BuildThreadCount=0,
+    [parameter(Mandatory=$false)]
+    [String] $BuildType="MinSizeRel"
+)
+
+### CONFIGURATION START ###
+$ErrorActionPreference = "Stop"
+
+if ( $BuildThreadCount -eq 0 ) {
+    $ComputerSystem = Get-CimInstance -class Win32_ComputerSystem
+    $BuildThreadCount = $ComputerSystem.NumberOfLogicalProcessors - 1
+    if ( $BuildThreadCount -le 0) {
+        $BuildThreadCount = 1
+    }
+}
+
+Write-Output "Build thread count: $BuildThreadCount"
+Write-Output "Build type: $BuildType"
+
+$WorkDir = $PSScriptRoot
+$BuildDir = "$WorkDir/build/desktop/"
+
+### CONFIGURATION END ###
+
+md $BuildDir -Force | Out-Null
+pushd $BuildDir
+
+cmake -G "Visual Studio 16 2019" -A x64 -Thost=x64 `
+  "$WorkDir"
+
+
+cmake --build "$BuildDir" --config $BuildType --parallel $BuildThreadCount
+
+cp $BuildDir/$BuildType/glm.clawed.dll $BuildDir/
+
+popd
