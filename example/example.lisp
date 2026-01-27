eefd5e0d6d778b0c38f00e938a3f6a546b351dcf
Pavel Korolev <dev@borodust.org>
2026-01-27 10:53:50 +0100

    Fix example

--- example/example.lisp
+++ example/example.lisp
@@ -5,93 +5,93 @@
 
 
 (defun vec4 (vec idx)
-  (cffi:mem-ref (%glm:glm-operator[] '(:pointer %glm:glm-vec4) vec :int idx) :float))
+  (cffi:mem-ref (%glm:glm+operator[] '(:pointer %glm:glm+vec4) vec :int idx) :float))
 
 
 (defun (setf vec4) (value vec idx)
-  (let ((ptr (%glm:glm-operator[] '(:pointer %glm:glm-vec4) vec :int idx)))
+  (let ((ptr (%glm:glm+operator[] '(:pointer %glm:glm+vec4) vec :int idx)))
     (setf (cffi:mem-ref ptr :float) (float value 0f0))))
 
 
 (defun make-vec4 (x y z w)
-  (let ((instance (iffi:make-intricate-instance '%glm:glm-vec4)))
-    (setf (vec4 instance 0) x
-          (vec4 instance 1) y
-          (vec4 instance 2) z
-          (vec4 instance 3) w)
+  (let ((instance (iffi:make-intricate-instance '%glm::glm+vec<4+float>)))
+    (setf (vec4 instance 0) (float x 0f0)
+          (vec4 instance 1) (float y 0f0)
+          (vec4 instance 2) (float z 0f0)
+          (vec4 instance 3) (float w 0f0))
     instance))
 
 
 (defun destroy-vec4 (vec)
-  (iffi:destroy-intricate-instance '%glm:glm-vec4 vec))
+  (iffi:destroy-intricate-instance '%glm:glm+vec4 vec))
 
 
 (declaim (inline vec4-add))
 (defun vec4-add (result this that)
-  (%glm:glm-operator+
-   '(:pointer %glm:glm-vec4) result
-   '(:pointer %glm:glm-vec4) this
-   '(:pointer %glm:glm-vec4) that))
+  (%glm:glm+operator+
+   '(:pointer %glm:glm+vec4) result
+   '(:pointer %glm:glm+vec4) this
+   '(:pointer %glm:glm+vec4) that))
 
 
 (declaim (inline vec4-mult))
 (defun vec4-mult (result this that)
-  (%glm:glm-operator*
-   '(:pointer %glm:glm-vec4) result
-   '(:pointer %glm:glm-vec4) this
-   '(:pointer %glm:glm-vec4) that))
+  (%glm:glm+operator*
+   '(:pointer %glm:glm+vec4) result
+   '(:pointer %glm:glm+vec4) this
+   '(:pointer %glm:glm+vec4) that))
 
 
 (declaim (inline vec4-dot))
 (defun vec4-dot (this that)
-  (%glm:glm-dot
-   '(:pointer %glm:glm-vec4) this
-   '(:pointer %glm:glm-vec4) that))
+  (%glm:glm+dot
+   '(:pointer %glm:glm+vec4) this
+   '(:pointer %glm:glm+vec4) that))
 
 
 (defun vec3 (vec idx)
-  (cffi:mem-ref (%glm:glm-operator[] '(:pointer %glm:glm-vec3) vec :int idx) :float))
+  (cffi:mem-ref (%glm:glm+operator[] '(:pointer %glm:glm+vec3) vec :int idx) :float))
 
 
 (defun (setf vec3) (value vec idx)
-  (let ((ptr (%glm:glm-operator[] '(:pointer %glm:glm-vec3) vec :int idx)))
+  (let ((ptr (%glm:glm+operator[] '(:pointer %glm:glm+vec3) vec :int idx)))
     (setf (cffi:mem-ref ptr :float) (float value 0f0))))
 
 
 (defun make-vec3 (x y z)
-  (let ((instance (iffi:make-intricate-instance '%glm:glm-vec3)))
-    (setf (vec4 instance 0) x
-          (vec4 instance 1) y
-          (vec4 instance 2) z)
+  (let ((instance (iffi:make-intricate-instance '%glm::glm+vec3)))
+    (setf (vec3 instance 0) (float x 0f0)
+          (vec3 instance 1) (float y 0f0)
+          (vec3 instance 2) (float z 0f0))
     instance))
 
 
 (defun destroy-vec3 (vec)
-  (iffi:destroy-intricate-instance '%glm:glm-vec3 vec))
+  (iffi:destroy-intricate-instance '%glm:glm+vec3 vec))
 
 
 (declaim (inline vec3-add))
 (defun vec3-add (result this that)
-  (%glm:glm-operator+
-   '(:pointer %glm:glm-vec3) result
-   '(:pointer %glm:glm-vec3) this
-   '(:pointer %glm:glm-vec3) that))
+  (%glm:glm+operator+
+   '(:pointer %glm:glm+vec3) result
+   '(:pointer %glm:glm+vec3) this
+   '(:pointer %glm:glm+vec3) that))
 
 
 (declaim (inline vec3-mult))
 (defun vec3-mult (result this that)
-  (%glm:glm-operator*
-   '(:pointer %glm:glm-vec3) result
-   '(:pointer %glm:glm-vec3) this
-   '(:pointer %glm:glm-vec3) that))
+  (%glm:glm+operator*
+   '(:pointer %glm:glm+vec3) result
+   '(:pointer %glm:glm+vec3) this
+   '(:pointer %glm:glm+vec3) that))
 
 
 (declaim (inline vec3-cross))
 (defun vec3-cross (result this that)
-  (%glm:glm-cross
-   '(:pointer %glm:glm-vec3) result
-   '(:pointer %glm:glm-vec3) this
-   '(:pointer %glm:glm-vec3) that))
+  (%glm:glm+cross
+   '(:pointer %glm:glm+vec3) result
+   '(:pointer %glm:glm+vec3) this
+   '(:pointer %glm:glm+vec3) that))
 
 
 ;;;
